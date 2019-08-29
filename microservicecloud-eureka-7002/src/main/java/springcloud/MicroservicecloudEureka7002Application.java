72e312c0aefe0bdbaa5d61d3b8ad09384fc52cde
admin <421585364@qq.com>
2019-08-29 15:23:06 +0800

    cloud

---
+++ microservicecloud-eureka-7002/src/main/java/springcloud/MicroservicecloudEureka7002Application.java
@@ -0,0 +1,13 @@
+package springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicecloudEureka7002Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicecloudEureka7002Application.class, args);
+    }
+
+}
